43a87b834ce20d6a12b6556e01fc092f6ac139ae
星野寛人 <hoshiro1998@icloud.com>
2020-05-03 23:55:36 +0900

    0503Commit_Instantiate_add

---
+++ Assets/Scripts/HikariWalkingAround.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HikariWalkingAround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1f;
+    public float rotationspeed = 1f;
+    public float posrange = 10f;
+    private Vector3 targetpos;
+    private float changetarget = 50f;
+    public float targetdistance;
+    Vector3 GetRandomPosition(Vector3 currentpos)
+    {
+        return new Vector3(Random.Range(-posrange + currentpos.x, posrange + currentpos.x), transform.position.y, Random.Range(-posrange + currentpos.z, posrange + currentpos.z));
+    }
+    void haikai()
+    {
+        if (targetdistance < changetarget) targetpos = GetRandomPosition(transform.position);
+        Quaternion targetRotation = Quaternion.LookRotation(targetpos - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationspeed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Debug.Log("haikai");
+    }
+    void Start()
+    {
+        targetpos = GetRandomPosition(transform.position);
+    }
+    void Update()
+    {
+        targetdistance = Vector3.SqrMagnitude(transform.position - targetpos);
+        haikai();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Hit");
+        print(other.gameObject.tag);// ログを表示する
+    }
+}
---
+++ Assets/Scripts/ProducingHikariRandom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProducingHikariRandom : MonoBehaviour
+{
+    public GameObject hikari_obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int r = 1; r <= 15; r++){
+       
+            Instantiate(hikari_obj, new Vector3(Random.Range(25.0f, 44.0f), hikari_obj.transform.position.y, Random.Range(-10.0f, 13.0f)), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
