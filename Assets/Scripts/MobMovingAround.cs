27497b5071d80b96f5565b6e777d2f680ae70976
星野寛人 <hoshiro1998@icloud.com>
2020-05-13 15:09:10 +0900

    0513Commit

---
+++ Assets/Scripts/MobMovingAround.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobMovingAround : MonoBehaviour
+{ // Start is called before the first frame update
+    public float speed = 1f;
+    public float rotationspeed = 1f;
+    public float posrange = 10f;
+    private Vector3 targetpos;
+    private float changetarget = 0.5f;
+    public float targetdistance;
+    // public GameObject particleObject;
+    //  private static Random random;
+
+    public Vector3 GetRandomPosition()
+    {
+        //return new Vector3(x_pos, transform.position.y, z_pos);
+        return new Vector3(Random.Range(-20f,20f), transform.position.y, Random.Range(-20f,20f));
+
+    }
+    void haikai()
+    {
+        if (targetdistance < changetarget)
+        {
+            targetpos = GetRandomPosition();
+            Quaternion targetRotation = Quaternion.LookRotation(targetpos - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationspeed);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Debug.Log("haikai");
+        }
+        else
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(targetpos - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationspeed);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            //print("haikai_else");
+        }
+
+    }
+    void Start()
+    {
+        targetpos = GetRandomPosition();
+
+    }
+    void Update()
+    {
+        //print("testdes");
+        targetdistance = Vector3.SqrMagnitude(transform.position - targetpos);//二乗を返している
+        this.transform.name = targetpos.ToString();
+        haikai();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "hikari")
+        {
+            print("ヒカリ徘徊");
+            haikai();
+        }
+
+        //Debug.Log("Hit");
+        print(other.gameObject.tag);// ログを表示する
+    }
+
+    public void HikariDestroy()
+    {
+        int k = int.Parse(this.gameObject.name);
+        if (0 < k)
+        {
+            Invoke("Hika_des", 3f);
+
+        }
+    }
+    void Hika_des()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void HikariSetPosition()
+    {
+        this.gameObject.transform.position = new Vector3(21.53f, -31.3f, 2.38f);
+        this.gameObject.transform.rotation = Quaternion.Euler(0f, 271.575f, 0f);
+        this.gameObject.GetComponent<HikariWalkingAround>().enabled = false;
+    }
+
+    public void ProduceParticle(GameObject particleObject)
+    {
+        Instantiate(particleObject, this.transform.position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/ProducingHikariRandom.cs
+++ Assets/Scripts/ProducingHikariRandom.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class ProducingHikariRandom : MonoBehaviour
 {
     public GameObject hikari_obj;
+    public GameObject mob_obj;
+    GameObject mobbox;
    // public GameObject tami;
     GameObject hika_obj;
-    Transform parent;
+    public GameObject parent_plane;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int r = 1; r <= 15; r++){
+        ProduceMob();
+    }
+
+    public void ProduceMob(){
+        for (int r = 1; r <= 120; r++)
+        {
+            mobbox = Instantiate(mob_obj, new Vector3(0, 0, 0), Quaternion.identity);
+            // mobbox.transform.localScale = new Vector3(0.075f, 0.075f, 0.075f);
+           // mobbox.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            mobbox.transform.position = new Vector3(Random.Range(-20f, 20f), mob_obj.transform.position.y, Random.Range(-20f, 20f));
+            mobbox.transform.rotation = Quaternion.Euler(0, Random.Range(0,360), 0);
+            StartCoroutine(MobAnimon(mobbox));
+        }
+    }
+
+    public IEnumerator MobAnimon(GameObject obj){
+        yield return new WaitForSeconds(0.1f);
+        obj.GetComponent<Animator>().enabled = true;
+        print(mobbox.name);
+
+    }
+
+    public void ProduceHikari(){
+        for (int r = 1; r <= 15; r++)
+        {
             //parent = this.transform;
             //  hika_obj = Instantiate(hikari_obj, new Vector3(Random.Range(25.0f, 44.0f), hikari_obj.transform.position.y, Random.Range(-10.0f, 13.0f)), Quaternion.identity, parent);
             hika_obj = Instantiate(hikari_obj, new Vector3(Random.Range(25.0f, 44.0f), hikari_obj.transform.position.y, Random.Range(-10.0f, 13.0f)), Quaternion.identity);
@@ -21,6 +47,7 @@ public class ProducingHikariRandom : MonoBehaviour
             //Hika_Restart();
             // tami.Getcomponent<
         }
+
     }
 
     // Update is called once per frame
